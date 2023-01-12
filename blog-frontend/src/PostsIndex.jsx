5f23d5cca677610c4764869f82df7ba7b482a2ca
johnryanmal <johnryanmal@gmail.com>
2023-01-12 11:08:38 -0800

    add post index cards

--- blog-frontend/src/PostsIndex.jsx
+++ blog-frontend/src/PostsIndex.jsx
@@ -1,13 +1,27 @@
 export function PostsIndex(props) {
+  function truncate(string, length) {
+    let ending = '...'
+    if (string <= length) {
+      return string
+    } else {
+      return string.substring(0, length - ending.length).trim() + ending
+    }
+  }
+
   return (
-    <div id="posts-index">
+    <div id="posts-index" className="container">
       <h1>All posts</h1>
 
-      {props.posts.map(recipe => (
-        <div key={recipe.id}>
-          <h2>{recipe.title}</h2>
-          <p>{recipe.body.length <= 200 ? recipe.body : recipe.body.substring(0, 200-3) + '...' }</p>
-          <div><button onClick={() => props.onSelect(recipe)}>Show Post</button></div>
+      {props.posts.map(post => (
+        <div class="card border-dark mb-3">
+          <div class="card-body">
+            <h5 class="card-title">{post.title}</h5>
+            <p class="card-text">{truncate(post.body,200)}</p>
+            <button type="button" class="btn btn-primary" onClick={() => props.onSelect(post)}>Show Post</button>
+          </div>
+          <div class="card-footer text-muted">
+            Posted at {new Date(post.created_at).toString()}
+          </div>
         </div>
       ))}
 
