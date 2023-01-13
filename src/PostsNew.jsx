b72bb90d27fef80075993c7a6387611830e1685d
johnryanmal <johnryanmal@gmail.com>
2023-01-13 12:11:10 -0800

    axios posts new

--- src/PostsNew.jsx
+++ src/PostsNew.jsx
@@ -1,12 +1,35 @@
+import axios from "axios";
+import { useState } from "react";
+
 export function PostsNew() {
+  const [errors, setErrors] = useState([])
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    let formData = new FormData(event.target)
+    axios.post("http://localhost:3000/posts.json")
+    .then((response) => {
+      console.log(response.data)
+    }).catch((error) => {
+      let errors = error.response?.data?.errors ?? [error.response?.statusText]
+      console.log(errors)
+      setErrors(errors)
+    })
+  }
+
   return (
     <div id="posts-new">
       <h1>New post</h1>
-      <form method="GET">
-        <div>Title: <input type="text" id="title"></input></div>
-        <div>Body: <input type="text" id="body"></input></div>
-        <div>Image: <input type="text" id="image"></input></div>
-        <input type="submit"></input>
+      <ul>
+        {errors.map((error, index) => (
+          <li key={index}>{error}</li>
+        ))}
+      </ul>
+      <form method="GET" onSubmit={handleSubmit}>
+        <div>Title: <input type="text" id="title" name="title"></input></div>
+        <div>Body: <input type="text" id="body" name="body"></input></div>
+        <div>Image: <input type="text" id="image" name="image"></input></div>
+        <button type="submit">Post</button>
       </form>
     </div>
   );
